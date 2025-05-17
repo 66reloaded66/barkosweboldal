47839ee0ea89df2c1f3faaf6affd6e6f6b8b168e
HUNBenya7328 <madarasz.bence07@gmail.com>
2025-05-17 21:01:03 +0200

    Kattintásra átdobás
    A galéria átdob mostantól a megfelelő lapokra, ha rányomunk a 'képekre'

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const galleries = document.querySelectorAll(".gallery");
-  
-    galleries.forEach((gallery, index) => {
-      const images = gallery.querySelectorAll(".gallery-img");
-      let current = 0;
-  
-      
-      const startDelay = index * 2000;
-  
-      setTimeout(() => {
-        setInterval(() => {
-          images[current].classList.remove("active");
-          current = (current + 1) % images.length;
-          images[current].classList.add("active");
-        }, 5000);
-      }, startDelay);
-    });
+  const galleries = document.querySelectorAll(".gallery");
+
+  galleries.forEach((gallery, index) => {
+    const images = gallery.querySelectorAll(".gallery-img");
+    let current = 0;
+
+
+    const startDelay = index * 2000;
+
+    setTimeout(() => {
+      setInterval(() => {
+        images[current].classList.remove("active");
+        current = (current + 1) % images.length;
+        images[current].classList.add("active");
+      }, 5000);
+    }, startDelay);
   });
-  
+});
+
+function openZomanc() {
+  window.location.href = '../html/kepnezegetok/zomanc.html';
+}
+
+function openFestmeny() {
+  window.location.href = '../html/kepnezegetok/festmeny.html';
+}
+
+function openVallasi() {
+  window.location.href = '../html/kepnezegetok/vallasi.html';
+}
+function openEkszerek() {
+  window.location.href = '../html/kepnezegetok/ekszerek.html';
+}
